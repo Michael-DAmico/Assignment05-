8c8785929956dcd030d8e6bc6f9aaa88e856e856
ChristianBurke03 <christian.burke@maine.edu>
2024-11-07 19:48:29 -0500

    Update Program6.java
    added javadoc

--- Program6.java
+++ Program6.java
@@ -1,14 +1,28 @@
 /**
- * @author Christian Burke and Michael D'Amico
- * @version 5 November 2024
- */
+* @author Christian Burke and Michael D'Amico
+* @version 7 November 2024
+*/
 package songpack;
 
-public class Program6 {
+import java.io.IOException;
+import java.util.ArrayList;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		if (args.length < 1) {
+/**
+ * The {@code program6} class is the entry point for running the search engine.
+ * It reads a TSV file containing song data, builds an index, and performs searches 
+ * for specified queries, measuring the time taken for each operation.
+ */
+public class program6 {
+    /**
+     * The main method reads a TSV file path from the command-line arguments, 
+     * initializes the search engine, and performs search queries.
+     *
+     * @param args command-line arguments where {@code args[0]} is expected to be 
+     *             the path to the TSV file containing song data
+     * @throws IOException if there is an error reading the TSV file
+     */
+    public static void main(String[] args) throws IOException {
+        if (args.length < 1) {
             System.out.println("Please provide the TSV file path.");
             return;
         }
@@ -30,17 +44,11 @@ public class Program6 {
             "put your hands in the jupitersky"
         };
         
-        for (String searchQuery : queries) {
+        for (String query : queries) {
             long searchStart = System.currentTimeMillis();
-            searchEngine.search(searchQuery);
+            searchEngine.search(query);
             long searchEnd = System.currentTimeMillis();
-            System.out.println((searchEnd - searchStart) + " milliseconds to search for \"" + searchQuery + "\"\n");
+            System.out.println((searchEnd - searchStart) + " milliseconds to search for \"" + query + "\"\n");
         }
-
-		
-		
-	}
-
-	}
-
+    }
 }
