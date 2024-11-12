3b83d6f3757844177af226e672e6f201e5799f39
ChristianBurke03 <christian.burke@maine.edu>
2024-11-11 20:53:03 -0500

    Update AVLTree.java

--- AVLTree.java
+++ AVLTree.java
@@ -1,81 +1,83 @@
-/**
- * @author Christian Burke and Michael D'Amico
- * @version 14 November 2024
- */
 package songpack;
 
-public class AVLTree extends BinarySearchTree{
-	private int leftRotation;
-	private int rightRotation;
-	private int leftRightRotation;
-	private int rightLeftRotation;
-	
-	
-	
-	public AVLTree() {
-		root = null;
-		leftRotation = 0;
-		rightRotation = 0;
-		leftRightRotation = 0;
-		rightLeftRotation = 0;
-	}
-	
-	public int getLeftRotation() {
-		return leftRotation;
-	}
-	
-	public int getRightRotation() {
-		return rightRotation;
-	}
-	
-	public int getLeftRightRotation() {
-		return leftRightRotation;
-	}
-	
-	
-	public int getRightLeftRotation() {
-		return rightLeftRotation;
-	}
-	
-	private void updateHeight(Node n) {
-		n.height = 1 + Math.max (height(n.left), height(n.right));
-	}
-	private Node leftRotation(Node current) {
-		Node replace = current.right;
-		Node replaceLeft = replace.left;
-		
-		replace.left = current;
-		current.right = replaceLeft;
-		
-		updateHeight(current);
-		updateHeight(replace);
-		return replace;
-		
-	}
-	
-	private Node rightRotation(Node current) {
-		Node replace = current.left;
-		Node replaceRight = replace.right;
-		
-		replace.right = current;
-		current.left = replaceRight;
-		
-		updateHeight(current);
-		updateHeight(replace);
-		return replace;
-		
-	}
-	
-		
-		/**
-		
-	@Override 
-	public void insert(Song data) {
-		return null;// fix later
-	}
-	
-	private Node insert(Node node, Song data) {
-		
-	}
-*/
+public class AVLTree extends BinarySearchTree {
+    private int leftRotations = 0;
+    private int rightRotations = 0;
+    private int leftRightRotations = 0;
+    private int rightLeftRotations = 0;
+
+    @Override
+    public void insert(Song item) {
+        root = insertAVL(root, item);
+    }
+
+    protected Node insertAVL(Node node, Song item) {
+        if (node == null) {
+            return new Node(item);
+        }
+
+        if (item.getViews() < node.data.getViews()) {
+            node.left = insertAVL(node.left, item);
+        } else {
+            node.right = insertAVL(node.right, item);
+        }
+
+        return balance(node);
+    }
+
+    private Node balance(Node node) {
+        int balanceFactor = getBalanceFactor(node);
+
+        if (balanceFactor > 1) {
+            if (getBalanceFactor(node.left) < 0) {
+                node.left = rotateLeft(node.left);
+                leftRightRotations++;
+            }
+            rightRotations++;
+            return rotateRight(node);
+        } else if (balanceFactor < -1) {
+            if (getBalanceFactor(node.right) > 0) {
+                node.right = rotateRight(node.right);
+                rightLeftRotations++;
+            }
+            leftRotations++;
+            return rotateLeft(node);
+        }
+        return node;
+    }
+
+    private Node rotateRight(Node y) {
+        Node x = y.left;
+        Node T2	 = x.right;
+        x.right = y;
+        y.left = T2;
+        return x;
+    }
+
+    private Node rotateLeft(Node x) {
+        Node y = x.right;
+        Node T2 = y.left;
+        y.left = x;
+        x.right = T2;
+        return y;
+    }
+
+    private int getBalanceFactor(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return getHeight(node.left) - getHeight(node.right);
+    }
+
+    int getAVLHeight(Node node) {
+        if (node == null) {
+            return 0;
+        }
+        return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+    }
+
+    public int getLeftRotations() { return leftRotations; }
+    public int getRightRotations() { return rightRotations; }
+    public int getLeftRightRotations() { return leftRightRotations; }
+    public int getRightLeftRotations() { return rightLeftRotations; }
 }
