2ff370e4a850e8c2b98f4cf2bcd34d771bcab692
Michael <michael.damico@maine.edu>
2024-11-11 20:20:27 -0500

    Create Program7.java

---
+++ Program7.java
@@ -0,0 +1,14 @@
+/**
+ * @author Christian Burke and Michael D'Amico
+ * @version 12 November 2024
+ */
+package songpack;
+
+public class Program7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
